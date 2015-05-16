e9dff9f756d4667917170cd4ecb0daf5c0f33050
Savannah Ostrowski <savannahostrowski@users.noreply.github.com>
2015-05-15 21:09:18 -0400

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -1,10 +1,7 @@
-#Port Scanner - Enter an IP address and a port range where the program will then attempt to find open ports on the given computer by connecting to each of them. On any successful connections mark the port as open.
-
 import socket
 
 hostname = socket.gethostname()
 ip = socket.gethostbyname(hostname)
-
 
 def isOpen(ip, port):
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,7 +12,5 @@ def isOpen(ip, port):
 	except:
 		print(str(port) + ': ' + 'Closed')
 
-isOpen(ip, 8000)
-
-# for i in range(1, 65535):
-# 	isOpen(ip, i)
+for i in range(1, 65535):
+	isOpen(ip, i)
